b91f720b7cb44888a9409a46d1050a67fa04f35e
Mike Trupkin <mike.trupkin@gmail.com>
2014-11-17 22:04:15 -0800

    xxx adding bintry client task

--- build.sbt
+++ build.sbt
@@ -1,3 +1,6 @@
+import bintry.Client
+import dispatch.as
+
 name := "console-lib"
 
 organization := "org.trupkin"
@@ -12,8 +15,21 @@ libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-swing" % "2.10.4"
 )
 
-addArtifact( Artifact("myproject", "dist", "zip"), buildLauncher)
-
 publishTo := Some(Resolver.file("file",  new File( Path.userHome.absolutePath + "/file-repo"))( Patterns("[artifact](-[classifier]).[ext]") ))
 
 publishMavenStyle := false
+
+lazy val publishBintray = taskKey[Unit]("Publish to Bintray")
+
+publishBintray := {
+  val user = "mtrupkin"
+  val bty = Client(user, "8189233b0eedd88871565ae0acbf641330e5394f")
+  val repo = bty.repo(user, "test")
+  val pack = repo.get("console-lib")
+  val ver = pack.version("0.0.2")
+  println("runPublishBintray")
+  val f = buildSfx.value
+  println(f.toString)
+  ver.upload(f.name, f).publish(true)(as.json4s.Json)
+  println("completed")
+}
--- project/bintry.scala
+++ project/bintry.scala
@@ -1,27 +1,28 @@
+import bintry._
+import dispatch.Defaults._
+import dispatch._
 import sbt._
-import Keys._
-import bintry._, dispatch._, dispatch.Defaults._, org.json4s._
 
 object BintryBuild extends Build {
 	val user = "mtrupkin"
-	val bty = Client(user, "XXX")
+	val bty = Client(user, "8189233b0eedd88871565ae0acbf641330e5394f")
 	val repo = bty.repo(user, "test")
 	val pack = repo.get("console-lib")
     //val ver1 = pack.createVersion("0.0.2")(as.json4s.Json)
     val ver = pack.version("0.0.2")
 
-	val publishBintray = TaskKey[Unit]("publish-bintray", "Publish to Bintray")
+	val publishBintray2 = TaskKey[Unit]("publish-bintray", "Publish to Bintray")
 
 	override lazy val settings = super.settings ++
 	    Seq(
-	      publishBintray := runPublishBintray.value
+	      publishBintray2 := runPublishBintray.value
 	    )
 
 	private def runPublishBintray: Def.Initialize[Task[Unit]] = Def.task {
 		println("runPublishBintray")
-		val f = new File(Path.userHome.absolutePath + "/file-repo", "console-lib-0.0.1.zip")
-		println(f.toString)
-		ver.upload("console-lib.zip", f).publish(true)(as.json4s.Json)
+		//val f = buildSfx.value
+		//println(f.toString)
+		//ver.upload(f.name, f).publish(true)(as.json4s.Json)
 		println("completed")
 	}
 }
